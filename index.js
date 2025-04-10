c9a2a86b5b981e7820afc1cd3e5d3bf32a28b855
Buzz Admin <team@buzzmarkets.io>
2025-04-10 17:48:24 -0400

    cors added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 // Health route
 app.get('/', (req, res) => {
   res.status(200).json({ status: 'OK', message: 'Server is running' });
